81f788f65fe0a5d945845317729f2483d0967f83
scoronel <scoronel@scoronel-K45VD>
2016-07-18 17:06:48 -0500

    cambios en return

--- apps/cajab/modules/consultas/actions/actions.class.php
+++ apps/cajab/modules/consultas/actions/actions.class.php
@@ -8,71 +8,66 @@
  * @author     Your name here
  * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
  */
-class consultasActions extends sfActions
-{
- /**
-  * Executes index action
-  *
-  * @param sfRequest $request A request object
-  */
-  public function executeIndex(sfWebRequest $request)
-  {
-      //echo "hola"; die();
-     $this->setLayout('index');
-   
-  }
-  public function executeGetCiclosEscolares(sfWebRequest $request) {
-  	try {  		
-  				
-  			//trae los ciclos porgramados y activos
-  			$ciclosEscolares = consultasInstituto::getCiclosEscolares();  			
-  			return $this->sendJSON($ciclosEscolares);
-  		
-  	} catch (Doctrine_Exception $e) {
-  		throw new sfException($e);
-  	}
-  }
-  
-  public function executeGetGradosByCiclo(sfWebRequest $request) {
-  	try {
-  		
-  			// hace un select distict de los grados por cada ciclo
-  			$idCiclo = $request->getParameter("idCiclo", 0);  			 
-  			$listaGrados = consultasInstituto::getGradosByCiclo($idCiclo);  			
-  			return $this->sendJSON($listaGrados);
-  	
-  	} catch (Doctrine_Exception $e) {
-  		throw new sfException($e);
-  	}
-  }
-  
-  public function executeGetGruposByGrado(sfWebRequest $request) {
-  	try {  		
-  			//consigue los grupos por el nombre del grado y por el ciclo
-  			$grado = $request->getParameter("grado", 0);
-  			$idCiclo = $request->getParameter("idCiclo", 0);
-  
-  			$listaGrupos = consultasInstituto::getGruposByGrado($grado, $idCiclo);  			
-  			return $this->sendJSON($listaGrupos);
-  		
-  	} catch (Doctrine_Exception $e) {
-  		throw new sfException($e);
-  	}
-  }
-  public function executeFiltrosAlumnos(sfWebRequest $request) {
-  	try {
-  		
-  			$idCiclo = $request->getParameter("idCiclo", 0);
-  			$idGrado = $request->getParameter("idGrado", 0);
-  			$idgrupo = $request->getParameter("idGrupo", 0);
-  			$alumno = $request->getParameter("alumno", 0); //busca por alumno se deben bloquear las demas opciones
-  			 
-  			$listaAlumnosQuery = consultasInstituto::getListaAlumnosFiltros($idCiclo, $idGrado, $idgrupo,$alumno);
-  			//print_r($listaAlumnosQuery);die();
-  			return $this->sendJSON($listaAlumnosQuery);
-  	
-  	} catch (Doctrine_Exception $e) {
-  		throw new sfException($e);
-  	}
-  }
+class consultasActions extends baseCajabProjectActions {
+
+    /**
+     * Executes index action
+     *
+     * @param sfRequest $request A request object
+     */
+    public function executeGetCiclosEscolares(sfWebRequest $request) {
+        try {
+
+            //trae los ciclos porgramados y activos
+            $ciclosEscolares = consultasInstituto::getCiclosEscolares();
+            $r = array("listaCiclos" => $ciclosEscolares); //a partir de php 5.4 es con corchetes[]                        
+            return $this->sendJSON($r);
+        } catch (Doctrine_Exception $e) {
+            throw new sfException($e);
+        }
+    }
+
+    public function executeGetGradosByCiclo(sfWebRequest $request) {
+        try {
+
+            // hace un select distict de los grados por cada ciclo
+            $idCiclo = $request->getParameter("idCiclo", 0);
+            $listaGrados = consultasInstituto::getGradosByCiclo($idCiclo);
+            $r = array("listaGrados" => $listaGrados); //a partir de php 5.4 es con corchetes[]                        
+            return $this->sendJSON($r);
+        } catch (Doctrine_Exception $e) {
+            throw new sfException($e);
+        }
+    }
+
+    public function executeGetGruposByGrado(sfWebRequest $request) {
+        try {
+            //consigue los grupos por el nombre del grado y por el ciclo
+            $grado = $request->getParameter("grado", 0);
+            $idCiclo = $request->getParameter("idCiclo", 0);
+
+            $listaGrupos = consultasInstituto::getGruposByGrado($grado,$idCiclo);
+            $r = array("listaGrupos" => $listaGrupos); //a partir de php 5.4 es con corchetes[]                        
+            return $this->sendJSON($r);
+        } catch (Doctrine_Exception $e) {
+            throw new sfException($e);
+        }
+    }
+
+    public function executeFiltrosAlumnos(sfWebRequest $request) {
+        try {
+
+            $idCiclo = $request->getParameter("idCiclo", 0);
+            $idGrado = $request->getParameter("idGrado", 0);
+            $idgrupo = $request->getParameter("idGrupo", 0);
+            $alumno = $request->getParameter("alumno", 0); //busca por alumno se deben bloquear las demas opciones
+
+            $listaAlumnosQuery = consultasInstituto::getListaAlumnosFiltros($idCiclo, $idGrado, $idgrupo, $alumno);
+            //print_r($listaAlumnosQuery);die();
+            return $this->sendJSON($listaAlumnosQuery);
+        } catch (Doctrine_Exception $e) {
+            throw new sfException($e);
+        }
+    }
+
 }
