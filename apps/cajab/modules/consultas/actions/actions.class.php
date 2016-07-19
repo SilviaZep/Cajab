a663a49ae312b7d9ad43437940da761cdcf0de21
scoronel <scoronel@scoronel-K45VD>
2016-07-19 01:06:05 -0500

    3.cambios en querys a prod

--- apps/cajab/modules/consultas/actions/actions.class.php
+++ apps/cajab/modules/consultas/actions/actions.class.php
@@ -46,7 +46,7 @@ class consultasActions extends baseCajabProjectActions {
             $grado = $request->getParameter("grado", 0);
             $idCiclo = $request->getParameter("idCiclo", 0);
 
-            $listaGrupos = consultasInstituto::getGruposByGrado($grado,$idCiclo);
+            $listaGrupos = consultasInstituto::getGruposByGrado($grado, $idCiclo);
             $r = array("listaGrupos" => $listaGrupos); //a partir de php 5.4 es con corchetes[]                        
             return $this->sendJSON($r);
         } catch (Doctrine_Exception $e) {
@@ -57,14 +57,35 @@ class consultasActions extends baseCajabProjectActions {
     public function executeFiltrosAlumnos(sfWebRequest $request) {
         try {
 
+            $limit = $request->getParameter("limit", 0);
+            $offset = $request->getParameter("offset", 0);
             $idCiclo = $request->getParameter("idCiclo", 0);
             $idGrado = $request->getParameter("idGrado", 0);
             $idgrupo = $request->getParameter("idGrupo", 0);
             $alumno = $request->getParameter("alumno", 0); //busca por alumno se deben bloquear las demas opciones
 
-            $listaAlumnosQuery = consultasInstituto::getListaAlumnosFiltros($idCiclo, $idGrado, $idgrupo, $alumno);
-            //print_r($listaAlumnosQuery);die();
-            return $this->sendJSON($listaAlumnosQuery);
+
+            $idServicio = $request->getParameter("idServicio", 0);
+            $idCategoria = $request->getParameter("idCategoria", 0); //si es false se toman en cuenta los que ya estan en el servicio            
+            $tipoTransporte = $request->getParameter("tipoTransporte", 0);
+
+            $flagVenta = false;
+            if ($idCategoria == 4 || ($idCategoria == 1 && $tipoTransporte == 3)) {//Ventas esta en id:4 hay que revisar cuando este en produccion
+                $flagVenta = true;
+            }
+            $idPapa = 0;
+            if (((int) $idServicio) > 0) {
+                $servicioForm = Doctrine::getTable('Servicio')->find((int) $idServicio);
+                $idPapa = $servicioForm->getIdServicio();
+            }
+
+
+            $listaAlumnosQuery = consultasInstituto::getListaAlumnosFiltros($idCiclo, $idGrado, $idgrupo, $alumno,$limit,$offset);
+            $totalListaAlumnosQuery = consultasInstituto::getTotalListaAlumnosFiltros($idCiclo, $idGrado, $idgrupo, $alumno);
+            $totalListaAlumnosQuery = $totalListaAlumnosQuery[0]['total'];
+            
+            $r = array("mensaje" => "Ok", "listaAlumnos" => $listaAlumnosQuery, "total" => $totalListaAlumnosQuery); //a partir de php 5.4 es con corchetes[]
+            return $this->sendJSON($r);
         } catch (Doctrine_Exception $e) {
             throw new sfException($e);
         }
